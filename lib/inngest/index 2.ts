dccefd462ac0c3e656f82b5bfa50eeab66d391a5
faizm10 <faizmustansar10@gmail.com>
2026-03-02 19:11:19 -0500

    deleted extra files

--- lib/inngest/index 2.ts
+++
@@ -1,4 +0,0 @@
-export { welcomeEmailFunction } from './functions/welcome';
-export { signInEmailFunction } from './functions/sign-in';
-
-export { reEngagementEmailFunction } from './functions/re-engagement';

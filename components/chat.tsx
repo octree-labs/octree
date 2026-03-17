e7f00230f2b90c3cf72ed7d4e2a30ca378831389
salarkhannn <salarkhanlala@gmail.com>
2026-03-17 22:39:02 +0500

    Cleanup: remove dead code

--- components/chat.tsx
+++
@@ -1,2 +0,0 @@
-// Re-export the modular Chat component
-export { Chat } from './chat/index';
--- types/document.ts
+++
@@ -1,8 +0,0 @@
-import { Tables } from '@/database.types';
-
-export type Document = Tables<'documents'> & {
-  projects?: {
-    id: string;
-    title: string;
-  } | null;
-};
